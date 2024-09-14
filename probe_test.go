d128c09be0a8ab88f87bbc1a2f158f66609cda61
linyows <linyows@gmail.com>
2024-09-14 14:18:36 +0900

    add test

---
+++ probe_test.go
@@ -0,0 +1,29 @@
+package probe
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"gopkg.in/yaml.v2"
+)
+
+func TestLoad(t *testing.T) {
+	p := &Probe{
+		FilePath: "./testdata/workflow.yml",
+		Log:      os.Stdout,
+		Verbose:  true,
+	}
+	err := p.Load()
+	if err != nil {
+		t.Errorf("probe load error %s", err)
+	}
+	expects, err := ioutil.ReadFile("./testdata/marshaled-workflow.yml")
+	if err != nil {
+		t.Errorf("file read error %s", err)
+	}
+	got, err := yaml.Marshal(p.workflow)
+	if string(got) != string(expects) {
+		t.Errorf("\nExpected:\n%s\nGot:\n%s", expects, got)
+	}
+}
