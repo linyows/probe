7fc2557d19f5802df75d931c9f7a77cfc22b9b8a
linyows <linyows@gmail.com>
2024-09-14 14:18:23 +0900

    add Load method and some struct for workflow

--- probe.go
+++ probe.go
@@ -1,4 +1,61 @@
 package probe
 
-func New() {
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/goccy/go-yaml"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+type Probe struct {
+	FilePath string
+	Log      io.Writer
+	Verbose  bool
+	workflow Workflow
+}
+
+type Step struct {
+	Name string            `yaml:"name"`
+	Use  string            `validate:"required"`
+	With map[string]string `yaml:"with"`
+}
+
+type Repeat struct {
+	Count    int `yaml:"count",validate:"required"`
+	Interval int `yaml:"interval"`
+}
+
+type Job struct {
+	Name   string `yaml:"name",validate:"required"`
+	Steps  []Step `yaml:"steps",validate:"required"`
+	Repeat Repeat `yaml:"repeat"`
+}
+
+type Workflow struct {
+	Name string `yaml:"name",validate:"required"`
+	Jobs []Job  `yaml:"jobs",validate:"required"`
+}
+
+func New() *Probe {
+	return &Probe{
+		FilePath: "./probe.yaml",
+		Log:      os.Stdout,
+		Verbose:  true,
+	}
+}
+
+func (p *Probe) Load() error {
+	y, err := ioutil.ReadFile(p.FilePath)
+	if err != nil {
+		return err
+	}
+	v := validator.New()
+	dec := yaml.NewDecoder(bytes.NewReader(y), yaml.Validator(v))
+	if err = dec.Decode(&p.workflow); err != nil {
+		return err
+	}
+	return nil
 }
