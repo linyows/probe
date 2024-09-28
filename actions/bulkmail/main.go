7bb8d887766ccec38348305fef327cb3044c3f9f
linyows <linyows@gmail.com>
2024-09-28 23:18:10 +0900

    add bulkmail of builtin-action

---
+++ actions/bulkmail/main.go
@@ -0,0 +1,36 @@
+package bulkmail
+
+import (
+	"os"
+
+	"github.com/hashicorp/go-hclog"
+	"github.com/hashicorp/go-plugin"
+	"github.com/linyows/probe"
+)
+
+type Action struct {
+	log hclog.Logger
+}
+
+func (a *Action) Run(args []string, with map[string]string) (map[string]string, error) {
+	a.log.Info("Hello!")
+	return with, nil
+}
+
+func Serve() {
+	log := hclog.New(&hclog.LoggerOptions{
+		Level:      hclog.Info,
+		Output:     os.Stderr,
+		JSONFormat: true,
+	})
+
+	pl := &probe.ActionsPlugin{
+		Impl: &Action{log: log},
+	}
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: probe.Handshake,
+		Plugins:         map[string]plugin.Plugin{"actions": pl},
+		GRPCServer:      plugin.DefaultGRPCServer,
+	})
+}
