9e8870d2f0328cd7a0e6e1e67647b1cae1575dae
linyows <linyows@gmail.com>
2024-09-30 18:59:02 +0900

    add AssignStruct

---
+++ reflect.go
@@ -0,0 +1,53 @@
+package probe
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func AssignStruct(pa ActionsParams, st any) error {
+	v := reflect.ValueOf(st).Elem()
+	t := v.Type()
+	e := &ValidationError{}
+
+	for i := 0; i < t.NumField(); i++ {
+		field := t.Field(i)
+		fType := field.Type
+		mapKey := field.Tag.Get("map")
+		va := field.Tag.Get("validate")
+		required := strings.Contains(va, "required")
+
+		if mapKey == "" {
+			continue
+		}
+
+		value, ok := pa[mapKey]
+		if ok {
+			switch fType.String() {
+			case "string":
+				v.Field(i).SetString(value)
+			case "int":
+				intValue, err := strconv.Atoi(value)
+				if err != nil {
+					e.AddMessage(fmt.Sprintf("params '%s' can't convert to int: %s", mapKey, err))
+				} else {
+					v.Field(i).SetInt(int64(intValue))
+				}
+			default:
+				e.AddMessage(fmt.Sprintf("params '%s' not found", mapKey))
+			}
+		}
+
+		if required && v.Field(i).String() == "" {
+			e.AddMessage(fmt.Sprintf("params '%s' is required", mapKey))
+		}
+	}
+
+	if e.HasError() {
+		return e
+	}
+
+	return nil
+}
