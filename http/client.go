d9bf7b82dfa3809d0b7fe602a042fc3faa49de66
linyows <linyows@gmail.com>
2024-10-21 18:51:40 +0900

    update http/client.go

--- http/client.go
+++ http/client.go
@@ -1,9 +1,11 @@
 package http
 
 import (
-	"io"
+	"bytes"
+	"errors"
+	"io/ioutil"
 	hp "net/http"
-	"net/url"
+	"strings"
 
 	"github.com/linyows/probe"
 )
@@ -14,53 +16,127 @@ type TransportOptions struct {
 }
 
 type Req struct {
-	Host    string            `map:"host" validate:"required"`
-	Path    string            `map:"path" validate:"required"`
-	Ver     string            `map:"ver"`
+	URL     string            `map:"url" validate"required"`
 	Method  string            `map:"method" validate:"required"`
-	Accept  string            `map:"accept"`
-	UA      string            `map:"ua"`
-	Data    []string          `map:"data"`
+	Proto   string            `map:"ver"`
 	Headers map[string]string `map:"headers"`
+	Body    []byte            `map:"body"`
+	cb      *Callback
 }
 
-func NewReq(p map[string]string) (*Req, error) {
-	r := &Req{
-		Ver:    "HTTP/1.1",
+type Res struct {
+	Headers map[string]string `map:"headers"`
+	Body    []byte            `map:"body"`
+}
+
+type Result struct {
+	Req Req `map:"req"`
+	Res Res `map:"res"`
+}
+
+func NewReq() *Req {
+	return &Req{
 		Method: "GET",
-		Accept: "*/*",
-		UA:     "probe/1.0.0",
+		Proto:  "HTTP/1.1",
+		Headers: map[string]string{
+			"Accept":     "*/*",
+			"User-Agent": "probe-http/1.0.0",
+		},
+	}
+}
+
+func (r *Req) Do() (*Result, error) {
+	if r.URL == "" {
+		return nil, errors.New("Req.URL is required")
 	}
 
-	if err := probe.AssignStruct(p, r); err != nil {
+	req, err := hp.NewRequest(r.Method, r.URL, bytes.NewBuffer(r.Body))
+	if err != nil {
 		return nil, err
 	}
 
-	return r, nil
-}
+	for k, v := range r.Headers {
+		req.Header.Set(k, v)
+	}
 
-func (r *Req) Do() ([]byte, error) {
-	url, err := url.JoinPath(r.Host, r.Path)
-	if err != nil {
-		return []byte{}, err
+	// callback
+	if r.cb != nil && r.cb.before != nil {
+		r.cb.before(req)
 	}
 
 	cl := &hp.Client{}
-
-	// d := strings.NewReader(*data)
-	req, err := hp.NewRequest(r.Method, url, nil)
-	if err != nil {
-		return []byte{}, err
-	}
-
-	req.Header.Set("Accept", r.Accept)
-	req.Header.Set("User-Agent", r.UA)
-
 	res, err := cl.Do(req)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 	defer res.Body.Close()
 
-	return io.ReadAll(res.Body)
+	// callback
+	if r.cb != nil && r.cb.after != nil {
+		r.cb.after(res)
+	}
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	headers := make(map[string]string)
+	for k, v := range res.Header {
+		// examples:
+		//   Set-Cookie: sessionid=abc123; Path=/; HttpOnly
+		//   Accept: text/html, application/xhtml+xml, application/xml;q=0.9
+		headers[k] = strings.Join(v, ", ")
+	}
+
+	return &Result{
+		Req: *r,
+		Res: Res{Headers: headers, Body: body},
+	}, nil
+}
+
+type Option func(*Callback)
+
+type Callback struct {
+	before func(req *hp.Request)
+	after  func(res *hp.Response)
+}
+
+func Request(data map[string]string, opts ...Option) (map[string]string, error) {
+	m := probe.UnflattenInterface(data)
+	r := NewReq()
+
+	cb := &Callback{}
+	for _, opt := range opts {
+		opt(cb)
+	}
+	r.cb = cb
+
+	if err := probe.MapToStructByTags(m, r); err != nil {
+		return map[string]string{}, err
+	}
+
+	ret, err := r.Do()
+	if err != nil {
+		return map[string]string{}, err
+	}
+
+	mapRet, err := probe.StructToMapByTags(ret)
+	if err != nil {
+		return map[string]string{}, err
+	}
+
+	return probe.FlattenInterface(mapRet), nil
+}
+
+func WithBefore(f func(req *hp.Request)) Option {
+	return func(c *Callback) {
+		c.before = f
+	}
+}
+
+func WithAfter(f func(res *hp.Response)) Option {
+	return func(c *Callback) {
+		c.after = f
+	}
 }
