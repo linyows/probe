ee3c5c7f4db4c7c725061c9962d598f445f74024
linyows <linyows@gmail.com>
2024-10-21 18:51:52 +0900

    update http/client_test.go

--- http/client_test.go
+++ http/client_test.go
@@ -8,23 +8,16 @@ import (
 )
 
 func TestNewReq(t *testing.T) {
-	p := map[string]string{
-		"host": "http://localhost:8080",
-		"path": "/foo/bar",
-	}
-
-	got, err := NewReq(p)
-	if err != nil {
-		t.Errorf("got error %s", err)
-	}
+	got := NewReq()
 
 	expects := &Req{
-		Host:   "http://localhost:8080",
-		Path:   "/foo/bar",
+		URL:    "",
 		Method: "GET",
-		Ver:    "HTTP/1.1",
-		Accept: "*/*",
-		UA:     "probe/1.0.0",
+		Proto:  "HTTP/1.1",
+		Headers: map[string]string{
+			"Accept":     "*/*",
+			"User-Agent": "probe-http/1.0.0",
+		},
 	}
 
 	if !reflect.DeepEqual(got, expects) {
@@ -32,22 +25,15 @@ func TestNewReq(t *testing.T) {
 	}
 }
 
-func TestReqDo(t *testing.T) {
+func TestDo(t *testing.T) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
 	res := httpmock.NewStringResponder(200, "Hello World\n")
 	httpmock.RegisterResponder("GET", "http://localhost:8080/foo/bar", res)
 
-	p := map[string]string{
-		"host": "http://localhost:8080",
-		"path": "/foo/bar",
-	}
-
-	req, err := NewReq(p)
-	if err != nil {
-		t.Errorf("got error %s", err)
-	}
+	req := NewReq()
+	req.URL = "http://localhost:8080/foo/bar"
 
 	got, err := req.Do()
 	if err != nil {
@@ -56,7 +42,7 @@ func TestReqDo(t *testing.T) {
 
 	expects := "Hello World\n"
 
-	if string(got) != expects {
-		t.Errorf("\nExpected:\n%s\nGot:\n%s", expects, got)
+	if string(got.Res.Body) != expects {
+		t.Errorf("\nExpected:\n%s\nGot:\n%s", expects, got.Res.Body)
 	}
 }
